c75c722a8ef7d6759b510296a75e4b4a3d2c5756
Isuka Wataliyadda <isuka1minjaya@gmail.com>
2025-06-16 21:03:42 +0530

    Add layout, page structure, and components for interview platform
    - Implement RootLayout with navigation and logo
    - Enhance page with AI-powered interview preparation section
    - Add DisplayTechIcons component to show technology icons
    - Create InterviewCard component to display interview details and feedback
    - Introduce utility functions for tech logo retrieval and random cover selection
    - Update package.json and package-lock.json to include dayjs dependency

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,47 @@
+import InterviewCard from '@/components/interviewCard'
+import { dummyInterviews } from '@/constants'
+import { Inter } from 'next/font/google'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Home Page
-    </div>
+    <>
+      <section className='card-cta'>
+        <div className='flex flex-col gap-6 max-w-lg'>
+          <h2>Get Interview Ready with AI-Powered Practice</h2>
+          <p className='text-lg'>
+            Practice on real interview questions & get AI-generated feedback to improve your skills.
+          </p>
+          <button className='btn-primary max-sw:w-full'>
+            <Link href="/interview">
+              Start an Interview
+            </Link>
+          </button>
+        </div>
+        <Image src="/robot.png" alt="robo-dude" width={400} height={400} className='max-sm:hidden' />
+       </section>
+
+       <section className='flex flex-col gap-6 mt-8'>
+          <h2>Your Interviews</h2>
+          <div className='interviews-section'>
+            {dummyInterviews.map((interview) => (
+              <InterviewCard{... interview} key={interview.id}/>
+            ))}
+          </div>
+        </section>
+
+        <section className='flex flex-col gap-6 mt-8'>
+          <h2>Take an Interview</h2>
+          <div className='interviews-section'>
+            {dummyInterviews.map((interview) => (
+              <InterviewCard{... interview} key={interview.id}/>
+            ))}
+            {/* <p>You haven't taken any interviews yet.</p> */}
+          </div>
+        </section>
+    </>
   )
 }
 
