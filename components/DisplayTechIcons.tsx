96fb337062483e5e5fc9e229920e6fab38d59fe6
Isuka Wataliyadda <isuka1minjaya@gmail.com>
2025-06-16 21:09:07 +0530

    Refactor DisplayTechIcons to include utility class names for dynamic styling

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { getTechLogos } from '@/lib/utils';
+import { cn, getTechLogos } from '@/lib/utils';
 import Image from 'next/image';
 
 const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
@@ -8,8 +8,8 @@ const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
 
   return (
     <div className='flex flex-row'>
-      {techIcons.slice(0, 3).map(({ tech, url }) => (
-        <div key={tech} className='relative group bg-dark-300 rounded-full p-2 flex-center items-center'>
+      {techIcons.slice(0, 3).map(({ tech, url }, index) => (
+        <div key={tech} className={cn('relative group bg-dark-300 rounded-full p-2 flex-center items-center', index >= 1 && 'ml-3')}>
             <span className='tech-tooltip'>
                 {tech}
             </span>
